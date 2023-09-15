091f7f56e3265a62657dd1adee33a38767fb0194
Seonjae Kim <theseonjae@gmail.com>
2023-09-15 15:39:53 -0500

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int score;
+    
+    cin >> score;
+    
+    if (90 <= score) {
+        cout << "A" << endl;
+    }
+    
+    else if (80 <= score) {
+        cout << "B" << endl;
+    }
+    
+    else if (70 <= score) {
+        cout << "C" << endl;
+    }
+    
+    else if (60 <= score) {
+        cout << "D" << endl;
+    }
+    
+    else {
+        cout << "F" << endl;
+    }
+    
+    return 0;
+}
